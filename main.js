0f0fd928651d82547504cca189f2e5b70cd537f4
Laufer4u <nzevgolis4u@gmail.com>
2021-01-02 00:54:37 +0200

    class Item

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 
-function addImage2Body(name, description, price) {
-    var src = document.createTextNode(name);
-    var description = document.createTextNode(' Description: ' + description);
-    var price = document.createTextNode(' Price: ' + price + '€');
+class Item {
+    constructor(name, description, price) {
+        let el_descr = document.createElement('div');
+        el_descr.innerHTML = ' Description: ' + description;
 
-    var x = document.createElement("img");
-    x.src = './img/'+name+'.jpg';
-    x.description = description;
-    x.price = price; 
+        let el_price = document.createElement('div');
+        el_price.innerHTML = ' Price: ' + price + '€';
+    
+        let el_img = document.createElement("img");
+        el_img.src = './img/'+name+'.jpg';
 
-    document.body.appendChild(x);
-    document.body.appendChild(x.description);
-    document.body.appendChild(x.price);
+        this.element = document.createElement('div');
+        this.element.appendChild(el_descr);
+        this.element.appendChild(el_price);
+        this.element.appendChild(el_img);
+    }
 }
+
+function addImage2Body(name, description, price) {
+
+    let item = new Item(name, description, price);
+    document.body.appendChild(item.element);
+}
+
 addImage2Body('2', 'Fruits ', '1000');
 addImage2Body('7', 'Ballet Shoes ', '800');
