1649e935e6aea3a049e68302aabd6a1aeba5f8b4
Laufer4u <nzevgolis4u@gmail.com>
2021-01-02 01:08:59 +0200

    File Item.js

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
 
-class Item {
-    constructor(name, description, price) {
-        let el_descr = document.createElement('div');
-        el_descr.innerHTML = ' Description: ' + description;
-
-        let el_price = document.createElement('div');
-        el_price.innerHTML = ' Price: ' + price + '€';
-    
-        let el_img = document.createElement("img");
-        el_img.src = './img/'+name+'.jpg';
-
-        this.element = document.createElement('div');
-        this.element.appendChild(el_descr);
-        this.element.appendChild(el_price);
-        this.element.appendChild(el_img);
-    }
-}
-
 function addImage2Body(name, description, price) {
 
     let item = new Item(name, description, price);
